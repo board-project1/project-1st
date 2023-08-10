c8e78218409fb0dbf93049b05a1a03257bbc8d14
Minhyeok Lee <122381580+Llimy1@users.noreply.github.com>
2023-08-10 22:57:01 +0900

    base code

---
+++ DDL.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `users` (
+  `email` VARCHAR(50) PRIMARY KEY,
+  `password` VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE `posts` (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `title` TEXT NOT NULL,
+  `content` LONGTEXT NOT NULL,
+  `author` VARCHAR(50) NOT NULL,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE TABLE `comments` (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `content` TEXT NOT NULL,
+  `author` VARCHAR(50) NOT NULL,
+  `post_id` INTEGER NOT NULL,
+  `created_at` timestamp
+);
+
+ALTER TABLE `posts` ADD FOREIGN KEY (`author`) REFERENCES `users` (`email`);
+
+ALTER TABLE `comments` ADD FOREIGN KEY (`author`) REFERENCES `users` (`email`);
+
+ALTER TABLE `comments` ADD FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
