427824e466a4904f6b30bce58ff7dd060d8831f0
hellokgang <137784468+hellokgang@users.noreply.github.com>
2023-08-15 22:10:39 +0900

    주석추가

--- src/main/java/com/supercoding/project_sample/controller/PostController.java
+++ src/main/java/com/supercoding/project_sample/controller/PostController.java
@@ -18,11 +18,17 @@ public class PostController {
 
     private final PostService postService;
 
+    // 게시물 전체 조회하는 API
+    // GET
     @GetMapping("/posts")
     public List<PostEntity> findPostList() {
         return postService.findPostList();
     }
 
+    // 작성자 이메일을 통해 특정 게시물들을 검색하는 API
+
+    // 게시물을 새롭게 만들 수 있는 API
+    // POST
     @PostMapping("/posts")
     public ResponseEntity<String> createPost(
             @RequestBody PostRequest postRequest) {
@@ -31,13 +37,17 @@ public class PostController {
         return ResponseEntity.ok("생성 성공.");
     }
 
+    // 기존 댓글의 글을 수정하는 API
+    // PUT
     @PostMapping("/posts/{postId}")
     public ResponseEntity<String> updatePost(
             @PathVariable Long postId,
             @RequestBody PostRequest postRequest) {
 
         postService.updatePost(postId, postRequest);
-        return ResponseEntity.ok("변경 성공.");
+        return ResponseEntity.ok("댓글이 성공적으로 수정되었습니다.");
+        // return 을 json 형태로 변경해야 하는건가??
+        // "message" :
     }
 
     @GetMapping("/{postId}/delete")
