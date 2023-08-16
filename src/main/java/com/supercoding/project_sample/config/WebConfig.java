ac237bc60763465f8e9595057cdb1c342c95befa
jiyoung-B <catgirl0313@naver.com>
2023-08-17 02:04:26 +0900

    cors 설정, 응답 수정(프론트에 회원가입/로그인 연결 확인 완료)
    front 수정사항:
    1. headers에 Content-Type 설정 추가
     headers: {
            "Content-Type": "application/json",
          },
    
    2. localStorage.setItem 값 변경
    ("email" -> "Authorization")

--- src/main/java/com/supercoding/project_sample/config/WebConfig.java
+++ src/main/java/com/supercoding/project_sample/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.supercoding.project_sample.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +16,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
         resolvers.add(memberAuthArgumentResolver);
+    }
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //모든 경로에 대해
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- src/main/java/com/supercoding/project_sample/controller/UserController.java
+++ src/main/java/com/supercoding/project_sample/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,24 +22,45 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController {
     private final UserService userService;
 
+//    @PostMapping("/signup")
+//    public ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest){
+//        boolean isSuccess = userService.signUp(signUpRequest);
+//        return isSuccess ? ResponseEntity.ok("회원가입이 완료되었습니다.") : ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 등록된 이메일입니다.");
+//    }
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest){
+    public ResponseEntity<Map<String, String>> signup(@RequestBody SignUpRequest signUpRequest){
         boolean isSuccess = userService.signUp(signUpRequest);
-        return isSuccess ? ResponseEntity.ok("회원가입이 완료되었습니다.") : ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 등록된 이메일입니다.");
+        if (isSuccess) {
+            return ResponseEntity.ok(Map.of("message", "회원가입이 완료되었습니다."));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", "이미 등록된 이메일입니다."));
+        }
     }
 
+//    @PostMapping("/login")
+//    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse) throws Exception {
+//        String token = userService.login(loginRequest);
+//
+//        if (token != null) {
+//            httpServletResponse.setHeader("Authorization", "Bearer " + token);
+//            return ResponseEntity.ok("로그인이 성공적으로 완료되었습니다.");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+//        }
+//    }
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse) throws Exception {
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse) throws Exception {
         String token = userService.login(loginRequest);
+        Map<String, String> response = new HashMap<>();
 
         if (token != null) {
             httpServletResponse.setHeader("Authorization", "Bearer " + token);
-            return ResponseEntity.ok("로그인이 성공적으로 완료되었습니다.");
+            response.put("message", "로그인이 성공적으로 완료되었습니다.");
+            return ResponseEntity.ok(response);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+            response.put("message", "로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
-
-
     }
 
     @PostMapping("/logout")
